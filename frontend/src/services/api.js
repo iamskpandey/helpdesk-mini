2e590aa5d26f532b803dc11f330187a5c2d37773
iamskpandey <188148706+iamskpandey@users.noreply.github.com>
2025-10-05 19:29:57 +0530

    backend url added fixed one

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: 'https://helpdesk-mini-aneq.onrender.com/',
+  baseURL: 'https://helpdesk-mini-aneq.onrender.com/api',
 });
 
 api.interceptors.request.use(
